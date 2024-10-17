9101a481f7cca6d9c88fdd5f405b1b13cd905215
ecipav <ecipav@gmail.com>
2024-10-17 21:12:28 +0300

    Create load-keypair.ts

---
+++ Laborator1/load-keypair.ts
@@ -0,0 +1,5 @@
+import "dotenv/config";
+import {getKeypairFromEnvironment} from "@solana-developers/helpers";
+const keypair = getKeypairFromEnvironment ("SECRET_KEY");
+console.log(`Loaded keypair from environment ${keypair.publicKey.toBase58()}`);
+console.log(`Loaded keypair from environment ${keypair.secretKey}`);
